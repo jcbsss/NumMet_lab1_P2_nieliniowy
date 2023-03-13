ef7e0cc9b226aba238f37c6cb1a9778ee00bebe6
jacobsss <kuba.spala@op.pl>
2023-03-13 17:17:50 +0100

    Jacobi matrix; beggining code

--- P2_nieliniowy/main.c
+++ P2_nieliniowy/main.c
@@ -9,30 +9,21 @@
 #include <math.h>
 
 void Constraints(double* x, double* F);		//To do
-void JacobiMatrix(double** J, double* x);		//To do
-void NewtonRaphson(double* x);		//To do
 
-void init_matrix(int N, double** A) {        //OLD  //Writes the A matrix
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			A[i][j] = 0.;
-
-	A[0][0] = 1.;  A[0][3] = 1.;
-	A[1][1] = 1.;  A[1][4] = 1.;
-	A[2][2] = 1.;  A[2][3] = -4.;  A[2][4] = 3.;
-	A[3][3] = -1.; A[3][5] = 1.;
-	A[4][4] = -1.; A[4][6] = 1.;
-	A[5][5] = -3.; A[5][6] = 4.;
-	A[6][5] = -1.; A[6][7] = 1.;
-	A[7][6] = -1.; A[7][8] = 1.;
-	A[8][7] = 7.;  A[8][8] = 2.;
+void JacobiMatrix(double** J, double* x) {
+	J[0][0] = 1; J[0][1] = 0; J[0][2] = 0; J[0][3] = 0;
+	J[1][0] = 0; J[1][1] = 1; J[1][2] = 0; J[1][3] = 0;
+	J[2][0] = -2*x[2] + 2*x[0]; J[2][1] = -2 * x[3] + 2 * x[1]; J[2][2] = -2 * x[0] + 2 * x[2]; J[0][3] = -2 * x[1] + 2 * x[3];
+	J[3][0] = 0; J[3][1] = 0; J[3][2] = 0; J[3][3] = 0;
 }
 
-void initial_approx(double* x) {		     //Initial approximation. It's my guess
-	b[0] = 2;
-	b[1] = 3.5;
-	b[2] = 2.7;
-	b[3] = 5.5;
+void NewtonRaphson(double* x);		//To do
+
+void initial_guess(double* x) {		     //Initial guess of x's
+	x[0] = 2;
+	x[1] = 3.5;
+	x[2] = 2.7;
+	x[3] = 5.5;
 }
 
 void print_vector(int N, double* b) {
@@ -40,31 +31,31 @@ void print_vector(int N, double* b) {
 		printf("%lf \n", b[i]);
 }
 
+
+
 int main() {
 	int N = 4;
-	double** A, * A_row;
 
-	double* x;
-	x = (double*)malloc(N * sizeof(double));
-	initial_approx(x);
+	double* x_old; double* x_new;					//allocation of old and new x's
+	x_old = (double*)malloc(N * sizeof(double));
+	x_new = (double*)malloc(N * sizeof(double));
+	initial_guess(x_old);							//initialization of the first approximation
 
-	//A = (double**)malloc(N * sizeof(double*));
-	//A_row = (double*)malloc(N * N * sizeof(double));
-	//for (int i = 0; i < N; i++) {
-	//	A[i] = &A_row[i * N];
-	//}
-	//init_matrix(N, A);
+	double** J, * J_row;							//allocation of the Jacobi matrix
+	J = (double**)malloc(N * sizeof(double*));
+	J_row = (double*)malloc(N * N * sizeof(double));
+	for (int i = 0; i < N; i++) {
+		J[i] = &J_row[i * N];
+	}
+	JacobiMatrix(J, x_old);							//initiation of the first Jacobi matrix
 
-	//double* b;
-	//b = (double*)malloc(N * sizeof(double));
-	//init_vector(N, b);
 
-	//double* x;
-	//x = (double*)malloc(N * sizeof(double));
+
+
 
 	
 
-	print_vector(N, x);
+	print_vector(N, x_new);
 
 	return 0;
 }
